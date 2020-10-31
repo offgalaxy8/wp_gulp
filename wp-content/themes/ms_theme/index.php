d8a47fe637bfcff70e2255da7e5da8026f306447
offgalaxy8 <56636426+offgalaxy8@users.noreply.github.com>
2020-11-01 00:06:18 +0300

    development content sections with styles

--- wp-content/themes/ms_theme/index.php
+++ wp-content/themes/ms_theme/index.php
@@ -1,8 +1,61 @@
 <?php get_header(); // вставка header.php ?>
 
     <div class="container">
-        <div class="content">
+        <div class="content cont-1">
             <h2>About Olynk</h2>
+            <div class="section-one">
+                <div class="so-1">
+                    <img src="/wp-content/uploads/2020/10/img-01.png" alt="image-test1">
+                    <div class="so-1-in">
+                        <p><span>Complete your Lynk</span></p>
+                        <p>Don't forget to add your photo & company logo. Click to upload a photo to help people in your proximity recognize you before they can Lynk with you.</p>
+                    </div>
+                </div>
+                <div class="so-2">
+                    <img src="/wp-content/uploads/2020/10/img-02.png" alt="image-test2">
+                    <div class="so-2-in">
+                        <p><span>Set Global Filters</span></p>
+                        <p>Targeted search will help find professionals based on desired job functions, in your proximity - to enable personalized professional networking.</p>
+                    </div>
+                </div>
+                <div class="so-3">
+                    <img src="/wp-content/uploads/2020/10/img-03.png" alt="image-test3">
+                    <div class="so-3-in">
+                        <p><span>Connect</span></p>
+                        <p>Share your Lynk for in-person networking. Build your personal and professional network organically. Feel free to add as many connections as you like.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section-two">
+        <h2>Get notified about launching</h2>
+        <form id="form" method="post">
+            <input type="text" name="us_name" placeholder="Enter your email here ">
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+
+    <div class="container">
+        <div class="content">
+            <h2>Our Team</h2>
+            <div class="section-three">
+                <div class="st-in">
+                    <img src="/wp-content/uploads/2020/10/photo-1.png" alt="photo">
+                    <p>Gaurav Saraf</p>
+                    <p><span>Product leader</span></p>
+                </div>
+                <div class="st-in">
+                    <img src="/wp-content/uploads/2020/10/photo-2.png" alt="photo">
+                    <p>Ankur Saraogi</p>
+                    <p><span>Product & Program Management</span></p>
+                </div>
+                <div class="st-in">
+                    <img src="/wp-content/uploads/2020/10/photo-2.png" alt="photo">
+                    <p>Piyush Gupta</p>
+                </div>
+            </div>
         </div>
     </div>
 
