40f5d9581f6017fb2692be037bb917cc2f77be7a
offgalaxy8 <56636426+offgalaxy8@users.noreply.github.com>
2020-11-01 00:37:54 +0300

    Add responsive styles

--- wp-content/themes/ms_theme/functions.php
+++ wp-content/themes/ms_theme/functions.php
@@ -1,7 +1,6 @@
 <?php
 function my_theme_enqueue_styles() {
-    $parent_style = 'parent-style';
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-sass', get_stylesheet_directory_uri() . '/build/css/child-styles.css', array(), '1.0', 'all' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/ms.css' );
+    wp_enqueue_style( 'parent-style-resp', get_template_directory_uri() . '/ms-responsive.css' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
--- wp-content/themes/ms_theme/header.php
+++ wp-content/themes/ms_theme/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html><head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="/wp-content/themes/ms_theme/ms.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head() ?>
 </head>
 <body>
